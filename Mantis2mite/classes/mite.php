e777ddb4647d77f8ada3fa4ed2b3ed7b8ff3c9d9
thomasklein <thomas.klein83@gmail.com>
2011-01-13 23:08:16 +0100

    mite.php: Changed HTTP protocol version for requests to 1.0 to avoid problems with chunked transfer encoding

--- Mantis2mite/classes/mite.php
+++ Mantis2mite/classes/mite.php
@@ -1,10 +1,9 @@
 <?php
-/* CLASS mite - last updated 23th December 2010
+/* CLASS mite - last updated 13th January 2011
  * 
  * @description provides methods to communicate with the MITE API
  * @package mite.plugins
  * @author Thomas Klein <thomas.klein83@gmail.com>
- * @version of mite.php 1.2.1
  * @license MIT License
  * 
  * Example usage:
@@ -151,7 +150,7 @@ class mite {
 		$s_fullUrl = $this->s_miteAccountUrl.$s_rsrcName;
 		
 	# begin to form the request	
-		$s_request = "$s_httpMethod $s_rsrcName HTTP/1.1\n".
+		$s_request = "$s_httpMethod $s_rsrcName HTTP/1.0\n".
 					 $this->s_header;
 	
 		switch ($s_httpMethod) {
